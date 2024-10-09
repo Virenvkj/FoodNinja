a4b6033abb8ebbd7020bfd0aab1c48611d7eeb2e
Viren Gajjar <vgajjar1010@gmail.com>
2024-10-09 11:03:40 +0530

    refactor: update name to booch maro

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food Ninja',
+      title: 'Booch maro',
       theme: ThemeData(
         // This is the theme of your application.
         //
