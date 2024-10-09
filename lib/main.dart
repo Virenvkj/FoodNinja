7549d9973836c7a21ff32abcd2e46f606808b502
Viren Gajjar <vgajjar1010@gmail.com>
2024-10-09 11:25:43 +0530

    refactor: update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'maro',
+      title: 'Food ninja',
       theme: ThemeData(
         // This is the theme of your application.
         //
