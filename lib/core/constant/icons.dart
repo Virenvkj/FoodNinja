9ae5136b7c22581ac4d30eb7fbafdf558e21834e
RAVAN <138647473+RAWAN3830@users.noreply.github.com>
2024-10-14 09:54:21 +0530

    feat:created constant_file

---
+++ lib/core/constant/icons.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Iconses{
+  static Icon home = const Icon(CupertinoIcons.house_fill);
+  static Icon cart = const Icon(CupertinoIcons.cart_fill);
+  static Icon chat = const Icon(CupertinoIcons.chat_bubble_2_fill);
+  static Icon person = const Icon(CupertinoIcons.person_alt);
+  static Icon back = const Icon(CupertinoIcons.back);
+  static Icon plus = const Icon(CupertinoIcons.add_circled);
+  static Icon minus = const Icon(CupertinoIcons.minus_circle);
+  static Icon delete = const Icon(CupertinoIcons.delete);
+  static Icon star = const Icon(CupertinoIcons.star_fill);
+  static Icon setting = const Icon(CupertinoIcons.settings);
+  static Icon credit_cart = const Icon(CupertinoIcons.creditcard);
+  static Icon information = const Icon(CupertinoIcons.info);
+  static Icon phone = const Icon(CupertinoIcons.phone);
+  static Icon message = const Icon(Icons.message_outlined);
+  static Icon search = const Icon(CupertinoIcons.search);
+  static Icon clear = const Icon(CupertinoIcons.clear);
+  static Icon attachment = const Icon(CupertinoIcons.pin_fill);
+  static Icon location = const Icon(Icons.place_outlined);
+  static Icon clock = const Icon(CupertinoIcons.clock_fill);
+
+}
