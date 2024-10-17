e8cc46bf7f3b74541fe760f415bbeba05e71afcf
RAVAN <138647473+RAWAN3830@users.noreply.github.com>
2024-10-17 16:41:26 +0530

    feat:create responsive framework

---
+++ lib/core/constant/theme_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class  ThemeColor{
+  static Color mainColor =  const Color(0xffFE8C00);
+  static Color buttonColor =  const Color(0xffFE8C00);
+  static Color iconColor =  const Color(0xffFE8C00);
+  static Color textColor = const Color(0xff000000);
+}
