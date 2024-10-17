47d6349bbaa97c054396261b4afd35e9087cce3c
RAVAN <138647473+RAWAN3830@users.noreply.github.com>
2024-10-17 21:42:49 +0530

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:foodninja/Presentation/Onboarding_Screen/onboarding_screen.dart';
+import 'package:foodninja/core/Theme_data/breakpoint_enum.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -10,13 +12,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Food Ninja',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const OnboardingScreen(),
+    return ResponsiveBreakpoints.builder(
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            title: 'Food Ninja',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const OnboardingScreen(),
+          );
+        }
+      ), breakpoints: [
+        BreakpointEnum.mobile.point,
+        BreakpointEnum.tablet.point,
+        BreakpointEnum.desktop.point,
+
+    ],
     );
   }
 }
